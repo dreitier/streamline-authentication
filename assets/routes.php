cad90117565267298d4be9aa51d9fed4c1cbf557
Christopher Klein <ckl@dreitier.com>
2023-02-21 15:39:57 +0100

    fix: wrong class reference
    Signed-off-by: Christopher Klein <ckl@dreitier.com>

--- assets/routes.php
+++ assets/routes.php
@@ -2,19 +2,12 @@
 
 declare(strict_types=1);
 
-use Dreitier\Streamline\Authentication\Facades\SocialiteMethodManager;
-use Dreitier\Streamline\Authentication\Facades\StreamlineAuthenticationMethod;
-use Dreitier\Streamline\Authentication\Methods\FormBased\Controllers\FormBasedController;
-use Dreitier\Streamline\Authentication\Methods\MagicLink\Controllers\MagicLinkController;
-use Dreitier\Streamline\Authentication\Methods\SelectableUser\Controllers\SelectableUserController;
-use Dreitier\Streamline\Authentication\Methods\Socialite\Controllers\SocialiteController;
-use Dreitier\Streamline\Authentication\Middlewares\OneTimeSignatureUsage;
-use Dreitier\Streamline\Authentication\Package;
+use Dreitier\Streamline\Authentication\Facades\StreamlineAuthentication;
 use Illuminate\Support\Facades\Route;
 
 // put everything in the web middleware so that we have our session. otherwise, the user would not be logged in
 Route::middleware(['web'])
     ->prefix('/sign-in')
     ->group(function () {
-        StreamlineAuthenticationMethod::defaultRoutes();
+        StreamlineAuthentication::defaultRoutes();
     });
